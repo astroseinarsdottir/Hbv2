48b0c42236dc1bfddd71d1bded7f04660352c3e9
astroseinarsdottir <astros.einars@gmail.com>
2017-03-20 17:54:36 +0000

    Profile activity til

--- YourHealthAndFitness/app/src/main/java/com/example/astrosei/yourhealthandfitness/HomePageActivity.java
+++ YourHealthAndFitness/app/src/main/java/com/example/astrosei/yourhealthandfitness/HomePageActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -19,10 +29,16 @@ public class HomePageActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
-    NavigationView navigationView;
-
+    private NavigationView navigationView;
+    private ListView listView;
 
     private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    int[] Images = {R.drawable.exercises,R.drawable.update_result,R.drawable.diet,
+            R.drawable.progress,R.drawable.profile};
+    String[] Titles = {"Exercise Program","Update Today","Diet Journal","Progress","Profile"};
+    String[] Descriptions = {"View your exercise program for the week","Insert result for today's exercise"
+            ,"View your diet program for the week","View your progress","View your profile"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +48,16 @@ public class HomePageActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        listView = (ListView)findViewById(R.id.listView);
+
+
+        // To set our special listView with images into the view.
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
 
         // Set our special toolbar as the action bar.
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Homepage");
+        getSupportActionBar().setTitle("Home page");
 
 
         // Creating the hamburger menu.
@@ -67,13 +89,81 @@ public class HomePageActivity extends AppCompatActivity {
 
             }
         });
+
+        // Redirects the user to the right activity when he clicks on it in the list.
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent;
+                switch (position){
+                    case 0:
+                        intent =  new Intent(HomePageActivity.this,ExercisesActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent =  new Intent(HomePageActivity.this,UpdateExerciseOfTodayActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent =  new Intent(HomePageActivity.this,DietJournalActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        intent =  new Intent(HomePageActivity.this,ProgressActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 4:
+                        intent =  new Intent(HomePageActivity.this,ProfileActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
 
-    // For the menu.
+    // For the navigation menu.
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
         actionBarDrawerToggle.syncState();
+    }
+
+
+    // Special class so the listView can have images.
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return Images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return Titles[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+
+            convertView = getLayoutInflater().inflate(R.layout.row_layout,null);
+
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.imageView);
+            TextView textView_Title = (TextView)convertView.findViewById(R.id.textView_Title);
+            TextView textView_Description = (TextView)convertView.findViewById(R.id.textView_Description);
+
+            imageView.setImageResource(Images[position]);
+            textView_Title.setText(Titles[position]);
+            textView_Description.setText(Descriptions[position]);
+
+            return convertView;
+        }
     }
 }
--- YourHealthAndFitness/app/src/main/java/com/example/astrosei/yourhealthandfitness/ProfileActivity.java
+++ YourHealthAndFitness/app/src/main/java/com/example/astrosei/yourhealthandfitness/ProfileActivity.java
@@ -1,13 +1,34 @@
 package com.example.astrosei.yourhealthandfitness;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private TextView textView_Name;
+    private TextView textView_Email;
+    private TextView textView_Age;
+    private TextView textView_Goal;
+    private TextView textView_Gender;
+    private TextView textView_Weight;
+    private Button  btn_Edit;
+
     private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
@@ -15,7 +36,78 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        textView_Name = (TextView)findViewById(R.id.textView_Name);
+        textView_Email = (TextView)findViewById(R.id.textView_Email);
+        textView_Age = (TextView)findViewById(R.id.textView_Age);
+        textView_Goal = (TextView)findViewById(R.id.textView_Goal);
+        textView_Gender = (TextView)findViewById(R.id.textView_Gender);
+        textView_Weight = (TextView)findViewById(R.id.textView_Weight);
+        btn_Edit = (Button)findViewById(R.id.btn_Edit);
+
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        //actionBarDrawerToggle = new ActionBarDrawerToggle((this, drawerLayout, ));
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+
+        // Set the right values into the view
+        // Not implemented, take values from database.
+        textView_Name.setText("Ástrós Einarsdóttir");
+        textView_Email.setText("Prufa1");
+        textView_Age.setText("Prufa2");
+        textView_Goal.setText("Prufa3");
+        textView_Gender.setText("Prufa4");
+        textView_Weight.setText("Prufa5");
+
+        // User wants to edit his profile, redirected to Update profile page.
+        btn_Edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, UpdateProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set our special toolbar as the action bar.
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Profile");
+
+        // Creating the hamburger menu.
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.drawer_open,R.string.drawer_close );
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        // Handler for navigation in the toolbar.
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()){
+                    case R.id.home_id:
+                        // Return to home page
+                        intent = new Intent(ProfileActivity.this, HomePageActivity.class);
+                        startActivity(intent);
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.logout_id:
+                        // Logout user, not fully implemented.
+                        intent = new Intent(ProfileActivity.this, IndexActivity.class);
+                        startActivity(intent);
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    // For the navigation menu.
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
     }
 }
