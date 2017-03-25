11ce8d039ad82f32f4f6de1be8c63aecb4cf9cbb
vitkor52 <viktorymir91@gmail.com>
2017-03-25 19:56:19 +0000

    new file

---
+++ YourHealthAndFitness/app/src/main/java/app/AppController.java
@@ -0,0 +1,8 @@
+package app;
+
+/**
+ * Created by viktor on 24/03/17.
+ */
+
+public class AppController {
+}
---
+++ YourHealthAndFitness/app/src/main/java/networker/UserNetworker.java
@@ -0,0 +1,8 @@
+package networker;
+
+/**
+ * Created by viktor on 24/03/17.
+ */
+
+public class UserNetworker {
+}
